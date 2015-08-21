a64a7e357d47f987061196dc0ae2722510b6689b
Mod Prakash <mod_prakash@infosys.com>
2015-08-21 18:55:11 +1000

    my assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this functions receives matrix as input argument and produces list of four functions 
+## as output
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+  inversematrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inversematrix <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inversematrix <<- inverse
+  getinverse <- function() inversematrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
+  
+  }
 
-## Write a short comment describing this function
+## this function uses output of above function as arguement and produces an inverse matrix
+## the inverse matrix is calculated for the first time only and any subsequent execution bring
+## out the value from the cached data only
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+           ## Return a matrix that is the inverse of 'x'
+      
+  inversematrix <- x$getinverse()
+  if(!is.null(inversematrix)) {
+    message("getting cached data")
+    return(inversematrix)
+  }
+  data <- x$get()
+  inversematrix <- solve(data, ...)
+  x$setinverse(inversematrix)
+  inversematrix
+       
+         
+       }
+
+## please use the following commands for the test of above two functions
+##  p1<- makeCacheMatrix(matrix(1:4,2,2))
+## cacheSolve(p1) - this will compute for the first time only
+## cacheSolve(p1) - this will fetch the cached data
+
+
+
+
